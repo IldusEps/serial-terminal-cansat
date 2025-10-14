cbad5f1ec512b53cab65ef99b3569d574538c188
IldusEps <ilduseps@mail.ru>
2025-10-14 16:36:42 +0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,44 @@
-import eslint from 'vite-plugin-eslint'
-import { VitePWA } from 'vite-plugin-pwa'
+import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default {
-  base: './',
+  base: "./",
   plugins: [
     eslint(),
     VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+      },
       manifest: {
-        short_name: 'Serial Terminal',
-        name: 'Serial Terminal',
+        short_name: "Serial Terminal",
+        name: "Serial Terminal",
         icons: [
           {
-            src: 'images/icons-1024.png',
-            type: 'image/png',
-            sizes: '1024x1024',
-            purpose: 'any maskable'
+            src: "images/icons-1024.png",
+            type: "image/png",
+            sizes: "1024x1024",
+            purpose: "any maskable",
           },
           {
-            src: 'images/icons-192.png',
-            type: 'image/png',
-            sizes: '192x192',
-            purpose: 'any maskable'
+            src: "images/icons-192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any maskable",
           },
           {
-            src: 'images/icons-512.png',
-            type: 'image/png',
-            sizes: '512x512',
-            purpose: 'any maskable'
-          }
+            src: "images/icons-512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
         ],
-        start_url: './?source=pwa',
-        display: 'standalone',
-        scope: './',
-        id: './'
+        start_url: "./?source=pwa",
+        display: "standalone",
+        scope: "./",
+        id: "./",
       },
-    })
-  ]
-}
+    }),
+  ],
+};
